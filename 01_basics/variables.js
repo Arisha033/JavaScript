103dc09d8b95ad8540bcde256b390b447d3e5318
Arisha <128699505+Arisha033@users.noreply.github.com>
2023-09-26 14:42:26 +0530

    variables in Js

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,1 +1,22 @@
+const myName = 'Arisha';
+console.log(myName);
+
+let myAge = 21;
+console.log(myAge);
+
+var myEmail = "Arishaa@gmail.com";
+console.log(myEmail);
+
+myCity = "Saharanpur";
+
+let myState;
+/*not allowed
+myName = "Warisha";
+*/
+
+myAge = 22;
+
+
+myEmail = "Arisha@gmail.com";
+console.table([myAge ,myEmail ,myName ,myCity ,myState ]);
 
