c878bfc12bc3638363f05bbc3f718b74a4fd5003
Arisha <128699505+Arisha033@users.noreply.github.com>
2023-09-27 18:34:00 +0530

    truthy and falsy values

---
+++ 04_control_flow/truthy&falsy.js
@@ -0,0 +1,43 @@
+const userEmail = [];
+
+if(userEmail){
+    // console.log("Got user email");
+}
+else{
+    // console.log("Don't have user email");
+}
+
+// falsy values
+// false, 0, -0, BigInt 0n, "" , null, undefined, NaN
+
+// truthy values
+// "0", "false", " ", [], {}, function(){}
+
+// if array is empty 
+
+ if(userEmail.length === 0) {
+//     console.log("Array is empty");
+ }
+
+// if object is empty
+
+const emptyObj = {}
+
+ if(Object.keys(emptyObj).length === 0) {
+//     console.log("Object is empty");
+ }
+
+// Nullish Coalescing Operator (??) : null undefined
+
+let val1;
+// val1 = 5 ?? 10;
+// val1 = null ?? 2;
+// val1 = 4 ?? undefined;
+// val1 = undefined ?? null;  // null
+// val1 = null ?? undefined;     // undefined
+val1 = null ?? 10 ?? 20;         // 10
+console.log(val1);
+
+// Terniary Operaator
+const iceTeaPrice = 100;
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
